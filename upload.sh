9bbe6f4b9e3dc549d525a5d402ebd368aade0968
bjyurkovich <bj.yurkovich@gmail.com>
2019-06-12 16:14:17 -0400

    upload script:

--- upload.sh
+++ upload.sh
@@ -2,8 +2,6 @@
 
 rm -rf iotery_embedded_python_sdk.egg-info/ && rm -rf build/ && rm -rf dist/ &&
 
-virtualenv test_env && source test_env/bin/activate &&
-
 python setup.py sdist bdist_wheel &&
 
 python -m pip install --upgrade twine &&
